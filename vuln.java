1419793e4bc26e320ea39e3b9382182c6e17af5e
Julio Arruda <julio.arruda@outlook.com>
2023-08-28 15:09:24 -0300

    teste

---
+++ vuln.java
@@ -0,0 +1,36 @@
+package com.scalesec.vulnado;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.JwtParser;
+	@@ -37,28 +39,26 @@ public static void assertAuth(String secret, String token) {
+  }
+
+  public static User fetch(String un) {
+    Statement stmt = null;
+    User user = null;
+    try {
+      Connection cxn = Postgres.connection();
+      stmt = cxn.createStatement();
+      System.out.println("Opened database successfully");
+
+      String query = "select * from users where username = '" + un + "' limit 1";
+      System.out.println(query);
+      ResultSet rs = stmt.executeQuery(query);
+      if (rs.next()) {
+        String user_id = rs.getString("user_id");
+        String username = rs.getString("username");
+        String password = rs.getString("password");
+        user = new User(user_id, username, password);
+      }
+      cxn.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.err.println(e.getClass().getName()+": "+e.getMessage());
+    } finally {
+      return user;
+    }
+  }
+}
